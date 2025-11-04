cdb61568e79974b08e1af57529f81148dbe326a0
sunkken <34925100+sunkken@users.noreply.github.com>
2025-11-04 20:05:39 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express')
 const Person = require('./models/person')
 
 const app = express()
-app.use(express.static('dist'))
-app.use(express.json())
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use((req, res, next) => {
@@ -15,6 +13,21 @@ app.use((req, res, next) => {
     morgan('tiny')(req, res, next)
   }
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(express.static('dist'))
+app.use(express.json())
+
+
 
 /* app.get('/info', (request, response) => {
   const requestTime = new Date()
@@ -30,10 +43,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -62,19 +81,15 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  
-  if (!persons.find(person => person.id === id)) {
-    return response.status(404).json({
-      error: 'person not found'
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
-  }
-
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+    .catch(error => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
